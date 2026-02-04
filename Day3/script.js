8856589f262c02a3c8eea760a09ee753bb7001b7
Abdul Kabir <abdulkabir0717@gmail.com>
2026-02-04 09:20:34 +0530

    Day3

---
+++ Day3/script.js
@@ -0,0 +1,19 @@
+
+let name1 = "maaz"
+let emp = {
+    name1 : "Kabir",
+    empDetails : function (){
+        console.log(this);
+        console.log(name1); 
+        console.log(this.name1);    
+    }
+}
+
+console.log(emp.empDetails());
+
+// var allow dublicates
+// var is global and function scope 
+// let and const have global, function and block scope
+
+// block scope are if,else,switch,do,while,{}
+
